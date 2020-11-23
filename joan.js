b1e2650740c7f0d34b222eccda4f5e982cb32493
Albert7452 <alberthincu@ginebro.cat>
2020-11-23 18:10:51 +0100

    Primer comit TAD

---
+++ joan.js
@@ -0,0 +1,27 @@
+let Persona = function(){
+    this.nom = nomEntrat;
+    this.congnom = congnomEntrat;
+};
+
+Persona.prototype.saluda=function(){
+    console.log('Hola, em dic '+ this.nom+' '+this.congnom);
+};
+Persona.prototype.setNom = function(nomNou){
+    this.nom=nomNou;
+}
+Persona.prototype.setCognom = function(cognomNou){
+    this.congnom= cognomNou;
+} 
+Persona.prototype.getNom = function (nomNou){
+    return this.nom;
+}
+Persona.prototype.getCognom = function(cognomNou){
+return this.congnom;
+}
+
+let traballador = new Persona("Albert","Hincu");
+let treballador2 = new Persona("Andi", "Hincu")
+
+treballador.saluda();
+
+treballador2.saluda();
